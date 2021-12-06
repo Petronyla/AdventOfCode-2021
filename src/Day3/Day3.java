7ef9156810b6967b7477b9146ebe839199b3904f
Petra Kubickova <kubicp10@fit.cvut.cz>
2021-12-06 14:38:39 +0100

    Add solution for a second part of Day 3.

--- src/Day3/Day3.java
+++ src/Day3/Day3.java
@@ -1,7 +1,10 @@
 package Day3;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Day3 {
@@ -34,8 +37,53 @@ public class Day3 {
         int epsilonRate = Integer.parseInt(sb.toString(), 2);
 
         int result = gammaRate * epsilonRate;
-        System.out.println(result);
+        System.out.println("First part: " + result);
 
+        int oxygen = Integer.parseInt(countOxygenGeneratorRating(instructionList, length), 2);
+        int CO2 = Integer.parseInt(countCO2ScrubberRating(instructionList, length), 2);
+
+        int result2 = oxygen * CO2;
+        System.out.println("Second part: " + result2);
+    }
+
+    private static String countOxygenGeneratorRating(List<String> list, int len) {
+        String res = null;
+        List<String> tmpList = new ArrayList<>(list);
+        for (int i = 0; i < len; i++) {
+            int mostCommonBit = findMostcommonBit(tmpList, i);
+            Iterator<String> iterator = tmpList.iterator();
+            while (iterator.hasNext() && tmpList.size() > 1) {
+                String tmp = iterator.next();
+                if (tmp.charAt(i) - '0' != (char) mostCommonBit) {
+                    iterator.remove();
+                }
+            }
+            if (tmpList.size() == 1) {
+                res = tmpList.get(0);
+                break;
+            }
+        }
+        return res;
+    }
+
+    private static String countCO2ScrubberRating(List<String> list, int len) {
+        List<String> tmpList = new ArrayList<>(list);
+        String res = null;
+        for (int i = 0; i < len; i++) {
+            int mostCommonBit = findMostcommonBit(tmpList, i);
+            Iterator<String> iterator = tmpList.iterator();
+            while (iterator.hasNext() && tmpList.size() > 1) {
+                String tmp = iterator.next();
+                if (tmp.charAt(i) - '0' == (char) mostCommonBit) {
+                    iterator.remove();
+                }
+            }
+            if (tmpList.size() == 1) {
+                res = tmpList.get(0);
+                break;
+            }
+        }
+        return res;
     }
 
     private static int findMostcommonBit(List<String> list, int position) {
@@ -47,10 +95,10 @@ public class Day3 {
                 cnt_0++;
         }
 
-        if (list.size() - cnt_0 > cnt_0)
+        if (list.size() - cnt_0 >= cnt_0)
             return 1;
-
-        return 0;
+        else
+            return 0;
     }
 
 }
